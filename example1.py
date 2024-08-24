e8e96746cedc3e49df0b32eb419d38cf50aa1d4d
nurbolot.dolatbekovich@gmail.com <nurbolot.dolatbekovich@gmail.com>
2024-08-24 17:07:45 +0300

    first commit

---
+++ example1.py
@@ -0,0 +1,24 @@
+def draw_frame(width, height):
+    # Проверяем, что ширина и высота больше 2
+    if width < 2 or height < 2:
+        print("Ширина и высота должны быть больше 2.")
+        return
+
+    # Строим верхнюю границу рамки
+    print('-' * width)
+
+    # Строим боковые границы
+    #for _ in range(height + 2):
+     #   print('|' + ' ' * (width - 2) + '|')
+
+    # Строим нижнюю границу рамки
+    print('-' * width)
+
+# Запрашиваем у пользователя ширину и высоту
+try:
+    user_width = int(input("Введите ширину рамки (минимум 2): "))
+    user_height = int(input("Введите высоту рамки (минимум 2): "))
+    
+    draw_frame(user_width, user_height)
+except ValueError:
+    print("Пожалуйста, введите корректные целые числа.")
